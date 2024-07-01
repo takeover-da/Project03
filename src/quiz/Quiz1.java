513f3f30efe41df17e81c4edacc96c18a3f374c1
takeover-da <124338334+takeover-da@users.noreply.github.com>
2024-07-02 00:56:17 +0900

    7월 1일 집에서 자바공부..

--- src/quiz/Quiz1.java
+++ src/quiz/Quiz1.java
@@ -1,19 +1,19 @@
-package quiz;
-
-public class Quiz1 {
-
-	public static void main(String[] args) {
-
-		int num = -10; 		 // -10
-		int result = +num;   // -10
-		
-		int num2 = -20;		 // -20
-		int result2 = -num2; // 20
-		
-		System.out.println("num: " + num);
-		System.out.println("reslut: " + result);
-		System.out.println("num2: " + num2);
-		System.out.println("result2: " + result2);
-	}
-
-}
+//package quiz;
+//
+//public class Quiz1 {
+//
+//	public static void main(String[] args) {
+//
+//		int num = -10; 		 // -10
+//		int result = +num;   // -10
+//
+//		int num2 = -20;		 // -20
+//		int result2 = -num2; // 20
+//
+//		System.out.println("num: " + num);
+//		System.out.println("reslut: " + result);
+//		System.out.println("num2: " + num2);
+//		System.out.println("result2: " + result2);
+//	}
+//
+//}
--- src/quiz/Quiz3.java
+++ src/quiz/Quiz3.java
@@ -1,15 +1,15 @@
-package quiz;
-
-public class Quiz3 {
-
-	public static void main(String[] args) {
-
-		int num = 456;
-		
-		int result = num / 100 * 100;
-		
-		System.out.println("결과: " + result);
-		
-	}
-
-}
+//package quiz;
+//
+//public class Quiz3 {
+//
+//	public static void main(String[] args) {
+//
+//		int num = 456;
+//
+//		int result = num / 100 * 100;
+//
+//		System.out.println("결과: " + result);
+//
+//	}
+//
+//}
